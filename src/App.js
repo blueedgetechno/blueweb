7676afe929ac339712f95ef3dff97cc7653cacca
BlueEdge <blueedgetechno@gmail.com>
2020-12-29 01:20:00 +0530

    cry

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
         <div className="empty"></div>
         <p>
           Under Construction <a href="https://github.com/blueedgetechno">{"</>"}</a>
-          <h1></h1>
         </p>
       </header>
     </div>
