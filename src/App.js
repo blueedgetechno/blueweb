32118df43da7a407f7f82c839a8937978ebd7e3f
BlueEdge <blueedgetechno@gmail.com>
2020-12-29 14:19:44 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="empty"></div>
+        <div className="empty" data-desc="we are doomed"></div>
         <p>
           Under Construction <a href="https://github.com/blueedgetechno">{"</>"}</a>
         </p>
